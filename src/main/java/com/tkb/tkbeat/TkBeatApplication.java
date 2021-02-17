53e628b352e73ea9f2f833b1e3db7491eb5e5129
bigkenbird <k918231524@gmail.com>
2021-02-17 23:03:46 +0800

    init commit

---
+++ src/main/java/com/tkb/tkbeat/TkBeatApplication.java
@@ -0,0 +1,13 @@
+package com.tkb.tkbeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TkBeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TkBeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tkb/tkbeat/TkBeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkb.tkbeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TkBeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
